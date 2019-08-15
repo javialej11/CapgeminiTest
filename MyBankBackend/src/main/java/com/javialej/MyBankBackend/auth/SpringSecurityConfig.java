06f405ae62d826d78fdea0f6c4b6fa2dabd3abb3
Javier Alejandro Perez Fernandez <javier.perez@amaris.com>
2019-08-15 03:28:10 -0500

    Authentication on Endpoints Added, Remain testing

--- MyBankBackend/src/main/java/com/javialej/MyBankBackend/auth/SpringSecurityConfig.java
+++ MyBankBackend/src/main/java/com/javialej/MyBankBackend/auth/SpringSecurityConfig.java
@@ -3,8 +3,11 @@ package com.javialej.MyBankBackend.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -19,10 +22,25 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 		
-	@Override
 	@Autowired
+	@Override	
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(this.customerService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean("authenticationManager")
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()		
+		.anyRequest().authenticated()
+		.and()
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
 }
--- MyBankBackend/src/main/java/com/javialej/MyBankBackend/controllers/CustomerController.java
+++ MyBankBackend/src/main/java/com/javialej/MyBankBackend/controllers/CustomerController.java
@@ -12,7 +12,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
